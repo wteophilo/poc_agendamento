02c490f0f2d71ae2978c8fe5b3cee23b04afc861
Willian Teophilo <wteophilo@gmail.com>
2016-07-29 19:56:27 -0300

    inicio

---
+++ src/main/java/br/com/wt/agendadoador/Boot.java
@@ -0,0 +1,14 @@
+package br.com.wt.agendadoador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(Boot.class, args);
+   }
+}
